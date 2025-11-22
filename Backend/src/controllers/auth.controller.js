d0d70ff0623a082aa9a39a56bad517904ddf34a3
tee-deb <solodey4uu@gmail.comm>
2025-11-22 06:24:00 +0100

    login user controller

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,9 +1,95 @@
-const express = require('express')
-
-const router = express.Router();
-
-
-router.post('/user/register', authController.registerUser)
+const userModel = require('../models/user.model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
-module.exports = router;
+
+async function registerUser(req , res) {
+
+    const { fullname , email , password } = req.body;
+
+    const isUserAlreadyExists = await userModel.findOne({
+         email })
+
+         if(isUserAlreadyExists) {
+            return res.status(400).json({
+                message:"User Already Exists"
+            })
+         }
+
+         const hashedPassword = await bcrypt.hash(password, 10);
+
+         const user = await userModel.create({
+            fullname,
+            email,
+            password: hashedPassword
+         })
+
+         const token = jwt.sign({
+            id: user._id
+         }, process.env.JWT_SECRET)
+
+            res.cookie("token", token)
+
+            res.status(201).json({
+                message: "User Registered Successfully",
+                user: {
+                    _id: user._id,
+                    email: user.email,
+                    fullname: user.fullname
+                }
+            })
+    
+}
+
+async function loginUser(req , res) {
+
+    const { email , password } = req.body;
+
+     const user = await userModel.findOne({
+        email
+     })
+
+     if(!user) {
+        res.status(400).json ({
+            message: "Invalid Email or Password"
+        })
+     }
+
+     const isPasswordValid = await bcrypt.compare(password , user.password);
+
+     if (!isPasswordValid) {
+        return res.status(400).json({
+            message: "Invalid Email or Password"
+        })
+     }
+
+     
+         const token = jwt.sign({
+            id: user._id
+         }, process.env.JWT_SECRET)
+
+            res.cookie("token", token)
+
+            res.status(200).json({
+                message: "User Logged In Successfully",
+                user: {
+                    _id: user._id,
+                    email: user.email,
+                    fullname: user.fullname
+                }
+            })
+}
+
+function logoutUser(req, res) {
+        res.clearCookie("token");
+        res.status(200).json ({
+            message: "User Logged Out Successfully"
+        });
+}
+
+module.exports = {
+    registerUser,
+     loginUser,
+    logoutUser,
+}
